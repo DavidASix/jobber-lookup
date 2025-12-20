71a0a71afaec28ad1d26b960ce76f563cb62d5bc
davidasix <david@davidasix.com>
2025-12-20 14:06:08 -0500

    Schemas are separated

---
+++ src/server/db/schema/posts.ts
@@ -0,0 +1,23 @@
+import { index, pgTable } from "drizzle-orm/pg-core";
+import { users } from "./auth";
+
+export const posts = pgTable(
+  "post",
+  (d) => ({
+    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+    name: d.varchar({ length: 256 }),
+    createdById: d
+      .varchar({ length: 255 })
+      .notNull()
+      .references(() => users.id),
+    createdAt: d
+      .timestamp({ withTimezone: true })
+      .$defaultFn(() => /* @__PURE__ */ new Date())
+      .notNull(),
+    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  }),
+  (t) => [
+    index("created_by_idx").on(t.createdById),
+    index("name_idx").on(t.name),
+  ]
+);
