bff900387bc3c4cfc715f28a5db9e1f1755cebf1
davidasix <david@davidasix.com>
2026-01-24 15:41:53 -0500

    Break on toast error

--- src/app/_dashboard/dashboard.tsx
+++ src/app/_dashboard/dashboard.tsx
@@ -45,6 +45,7 @@ export function Dashboard({ user }: { user: User }) {
       toast.error(
         "Authorization URL not available. Please refresh and try again.",
       );
+      return;
     }
     setIsAuthorizing(true);
     window.open(authUrl, "_blank");
