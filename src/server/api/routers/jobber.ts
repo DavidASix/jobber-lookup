6a2cd7fa4f492abc49451c365a6f9a86043e1e2c
davidasix <david@davidasix.com>
2026-01-24 12:18:30 -0500

    Add endpoints to get authorization URL

--- src/server/api/routers/jobber.ts
+++ src/server/api/routers/jobber.ts
@@ -4,8 +4,46 @@ import { eq } from "drizzle-orm";
 import { db } from "~/server/db";
 import { accountData } from "~/lib/jobber/graphql";
 import { getJobberAccessToken } from "~/lib/jobber/access-tokens";
+import { urls } from "~/lib/jobber/utils";
+import { env } from "~/env";
 
 export const jobberRouter = createTRPCRouter({
+  getPublicId: protectedProcedure.query(async ({ ctx }) => {
+    const { id: user_id } = ctx.session.user;
+
+    const [account] = await db
+      .select({ public_id: jobberAccounts.public_id })
+      .from(jobberAccounts)
+      .where(eq(jobberAccounts.user_id, user_id));
+
+    return account?.public_id ?? null;
+  }),
+
+  /**
+   * Stores a new random state associated with the current user and returns the full Jobber OAuth authorization URL.
+   */
+  getAuthorizationUrl: protectedProcedure.query(async ({ ctx }) => {
+    const { id: user_id } = ctx.session.user;
+    const state = crypto.randomUUID();
+    await db.insert(authenticationState).values({
+      state,
+      user_id,
+      valid: true,
+    });
+    const client_id = env.NEXT_PUBLIC_JOBBER_CLIENT_ID;
+    const base_url = env.NEXT_PUBLIC_PROJECT_URL;
+    const redirect_uri = `${base_url}/api/jobber-connect/authorize`;
+
+    const queryString = new URLSearchParams({
+      response_type: "code",
+      client_id,
+      redirect_uri,
+      state,
+    }).toString();
+
+    return `${urls.oauth}?${queryString}`;
+  }),
+
   getState: protectedProcedure.query(async ({ ctx }) => {
     const { id: user_id } = ctx.session.user;
     const state = crypto.randomUUID();
